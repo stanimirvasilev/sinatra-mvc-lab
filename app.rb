081a8bf9c5f330a4ef78035982784a672b5de00e
+ <+>
2022-01-24 08:15:16 +0000

    commit tree 8d56221737efa837f2c974c69a8abae7f0af3145

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+
+    get '/' do
+        erb :user_input
+      end
+
+      post '/piglatinize' do
+        user_word = params["user_phrase"]
+        word = PigLatinizer.new
+        @piglatinized_word = word.piglatinize(user_word)
+      end
+
+
 end
